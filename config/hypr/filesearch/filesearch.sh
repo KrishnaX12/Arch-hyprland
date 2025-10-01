cb6176416e59aa97e84f52c6cfccec82b818f637
Krishna Raj B <bkr.2007@yahoo.com>
2025-10-01 18:44:02 +0530

    Delete config directory for updating it

--- config/hypr/filesearch/filesearch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# File search with custom CSS
-
-FILE=$(fd --type f . $HOME 2>/dev/null | wofi --show dmenu --prompt "Search file:" --style ~/.config/hypr/filesearch/style.css)
-
-if [ -n "$FILE" ]; then
-    xdg-open "$FILE"
-fi
--- config/hypr/keyBindings/show_keybinds.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-konsole --new-tab -e bash -c "
-  glow ~/.config/hypr/keyBindings/keybinds.md;
-  echo -e '\n\033[1;32mPress ENTER to close...\033[0m';
-  read
-"
-
